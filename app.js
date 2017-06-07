582642a7f2868b561718a3fc82fd9215a364409b
Evelyn Sharp <yihongshi90@gmail.com>
2017-06-06 19:14:57 -0600

    add heroku

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy
 const mongoose = require('mongoose');
+const dbUrl = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost//eventech-capstone';
 mongoose.connect('mongodb://localhost/meetup-capstone');
 
 const app = express();
