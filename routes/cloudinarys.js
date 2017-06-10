836e3c75fb47d83df431557c14b442fb38ed8f43
Evelyn Sharp <yihongshi90@gmail.com>
2017-06-10 09:11:09 -0600

    dropzone init

--- routes/cloudinarys.js
+++ routes/cloudinarys.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const Cloudinary = require('cloudinary');
+const cloudinary = require('cloudinary');
 
 const client = () => {
-  return new Cloudinary({
+  return cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
