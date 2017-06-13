2e63b75615223c66a8acbd25448448447f43f31d
Evelyn Sharp <yihongshi90@gmail.com>
2017-06-12 18:45:52 -0600

    image selection to action file

--- client/src/actions/events.js
+++ client/src/actions/events.js
@@ -1,3 +1,54 @@
+import  Business1 from '../images/Business1.jpg';
+import  Business2 from '../images/Business2.jpg';
+import  Business3 from '../images/Business3.jpg';
+import  Food1 from '../images/Food1.jpg';
+import  Food2 from '../images/Food2.jpg';
+import  Food3 from '../images/Food3.jpg';
+import  Music1 from '../images/Music1.jpg';
+import  Music2 from '../images/Music2.jpg';
+import  Music3 from '../images/Music3.jpg';
+import  Other1 from '../images/Other1.jpg';
+import  Other2 from '../images/Other2.jpg';
+import  Other3 from '../images/Other3.jpg';
+import  Science1 from '../images/Science1.jpg';
+import  Science2 from '../images/Science2.jpg';
+import  Science3 from '../images/Science3.jpg';
+import  Sports1 from '../images/Sports1.jpg';
+import  Sports2 from '../images/Sports2.jpg';
+import  Sports3 from '../images/Sports3.jpg';
+
+const imageSet={
+  Business: [ Business1, Business2, Business3 ],
+  Food: [ Food1, Food2, Food3 ],
+  Music: [ Music1, Music2, Music3 ],
+  Other: [ Other1, Other2, Other3 ],
+  Science: [ Science1, Science2, Science3 ],
+  Sports: [ Sports1, Sports2, Sports3 ]
+}
+
+const decideImage = (category, imageIndex) => {
+  switch(category) {
+    case 'Business':
+      return imageSet.Business[imageIndex]
+    case 'Sports & Fitness':
+      return imageSet.Sports[imageIndex]
+    case 'Science & Tech':
+      return imageSet.Science[imageIndex]
+    case 'Music & Arts':
+      return imageSet.Music[imageIndex]
+    case 'Food & Drink':
+      return imageSet.Food[imageIndex]
+    default:
+      return imageSet.Other[imageIndex]
+  }
+}
+
+export const randomImageSelection = (category) => {
+  let imageIndex = Math.floor((Math.random()*3));
+  return decideImage(category, imageIndex);
+}
+
+// above for random image selection
 
 export const getEvents = () => {
   return(dispatch) => {
--- client/src/components/EventForm.js
+++ client/src/components/EventForm.js
@@ -1,26 +1,8 @@
 import React, { Component } from 'react';
 import { Header, Form, Button, Select, Message } from 'semantic-ui-react';
 import { connect } from 'react-redux';
-import { addEvent, updateEvent } from '../actions/events';
+import { addEvent, updateEvent, randomImageSelection } from '../actions/events';
 import { categoryOptions } from '../categoryOptions';
-import  Business1 from '../images/Business1.jpg';
-import  Business2 from '../images/Business2.jpg';
-import  Business3 from '../images/Business3.jpg';
-import  Food1 from '../images/Food1.jpg';
-import  Food2 from '../images/Food2.jpg';
-import  Food3 from '../images/Food3.jpg';
-import  Music1 from '../images/Music1.jpg';
-import  Music2 from '../images/Music2.jpg';
-import  Music3 from '../images/Music3.jpg';
-import  Other1 from '../images/Other1.jpg';
-import  Other2 from '../images/Other2.jpg';
-import  Other3 from '../images/Other3.jpg';
-import  Science1 from '../images/Science1.jpg';
-import  Science2 from '../images/Science2.jpg';
-import  Science3 from '../images/Science3.jpg';
-import  Sports1 from '../images/Sports1.jpg';
-import  Sports2 from '../images/Sports2.jpg';
-import  Sports3 from '../images/Sports3.jpg';
 import EventImageDrop from './EventImageDrop';
 
 
@@ -28,16 +10,6 @@ class EventForm extends Component {
   defaultData = { eventName: '', organizer: '', date: '', location: 'current location', category: '', description: '', attendeeIds: [], imageUrl:'', updateEvent: false, categoryCheck: true }
 
   state={  ...this.defaultData  }
-
-  imageSet={
-    Business: [ Business1, Business2, Business3 ],
-    Food: [ Food1, Food2, Food3 ],
-    Music: [ Music1, Music2, Music3 ],
-    Other: [ Other1, Other2, Other3 ],
-    Science: [ Science1, Science2, Science3 ],
-    Sports: [ Sports1, Sports2, Sports3 ]
-  }
-
 
   componentDidMount = () => {
     if(this.props.updateEvent) {
@@ -57,8 +29,8 @@ class EventForm extends Component {
     let { _id, username, history } = this.props;
     let imageDisplay;
     if(this.state.imageUrl === '') {
-      let imageIndex = Math.floor((Math.random()*3));
-      imageDisplay = this.decideImage(this.state.category, imageIndex);
+      imageDisplay= randomImageSelection(this.state.category);
+
     } else {
       imageDisplay = this.state.imageUrl;
     }
@@ -81,23 +53,6 @@ class EventForm extends Component {
         this.setState({ ...this.defaultData });
         this.props.toggleEdit();
       }
-  }
-
-  decideImage = (category, imageIndex) => {
-    switch(category) {
-      case 'Business':
-        return this.imageSet.Business[imageIndex]
-      case 'Sports & Fitness':
-        return this.imageSet.Sports[imageIndex]
-      case 'Science & Tech':
-        return this.imageSet.Science[imageIndex]
-      case 'Music & Arts':
-        return this.imageSet.Music[imageIndex]
-      case 'Food & Drink':
-        return this.imageSet.Food[imageIndex]
-      default:
-        return this.imageSet.Other[imageIndex]
-    }
   }
 
   setImageUrlState = (imageUrl) => {
