b0ec2a2cca196cd980fa1e8188f6663e9907ace2
Evelyn Sharp <yihongshi90@gmail.com>
2017-06-24 18:24:23 -0600

    login bg

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -3,20 +3,21 @@ import Login from './Login';
 import { Modal, Button, Grid, Segment } from 'semantic-ui-react';
 
 const LoginPage = ({ ...props }) => (
-  <div style={{ margin: '0 30%'}}  >
-    <Segment style={{ marginTop: '1em'}}>
-      <Grid style={{  padding: '3%'}}>
-        <Grid.Row centered >
-          <Grid.Column width={1} />
-          <Grid.Column width={14} style={{ textAlign: 'center'}}>
-            <Login {...props}/>
-          </Grid.Column>
-          <Grid.Column width={1} />
-        </Grid.Row>
-      </Grid>
-    </Segment>
+  <div className="loginPageCon">
+    <div style={{ margin: '13% 30%'}}  >
+      <Segment>
+        <Grid style={{  padding: '3%'}}>
+          <Grid.Row centered >
+            <Grid.Column width={1} />
+            <Grid.Column width={14} style={{ textAlign: 'center'}}>
+              <Login {...props}/>
+            </Grid.Column>
+            <Grid.Column width={1} />
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    </div>
   </div>
-
 )
 
 export default LoginPage;
